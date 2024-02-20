b95fc681a13427d6565e1b7632181ec652347c52
GaurangGatlewar <gvg228@nyu.edu>
2024-02-19 22:59:08 -0500

    Better solution with pointers

--- threeSum.java
+++ threeSum.java
@@ -1,90 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        int count = 0;
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int i: nums){
-            if(map.containsKey(i)){
-                count = map.get(i)+1;
-            } else {
-                count = 1;
+    public  List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res  = new HashSet<>();
+        if(nums.length==0) return new ArrayList<>(res);
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2;i++){
+            int j =i+1;
+           int  k = nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+                else if (sum >0) k--;
+                else if (sum<0) j++;
             }
-            map.put(i, count);
-        }
 
-        HashSet<List<Integer>> answer = new HashSet<>();
-        List<Integer> temp = new ArrayList<Integer>();
-        Integer[] numList = map.keySet().toArray(new Integer[map.size()]);
-        if (map.containsKey(0)){
-            if (map.get(0)>2){
-                temp.add(0);
-                temp.add(0);
-                temp.add(0);
-                answer.add(temp);
-            }
         }
-
-        for(int i=0;i<numList.length;i++){
-            if (map.get(numList[i])>1){
-                int num = numList[i]*(-2);
-                if(num==0) continue;
-                if (map.containsKey(num)){
-                    temp = new ArrayList<Integer>();
-                    if(num>numList[i]){
-                        temp.add(numList[i]);
-                        temp.add(numList[i]);
-                        temp.add(num);
-                    } else {
-                        temp.add(num);
-                        temp.add(numList[i]);
-                        temp.add(numList[i]);
-                    }
-                    answer.add(temp);
-                }
-            }
-        }
-
-        for(int i=0;i<numList.length;i++){
-            for(int j=i+1;j<numList.length;j++){
-                int num = -numList[i]-numList[j];
-                if(map.containsKey(num)){
-                    if((num != numList[i]) && (num != numList[j])){
-                        temp = new ArrayList<Integer>();
-                        if(num<numList[i] && num<numList[j]){
-                            temp.add(num);
-                            if(numList[i]<numList[j]){
-                                temp.add(numList[i]);
-                                temp.add(numList[j]);
-                            } else {
-                                temp.add(numList[j]);
-                                temp.add(numList[i]);
-                            }
-                        } else if(numList[i]<numList[j]){
-                            temp.add(numList[i]);
-                            if(num<numList[j]){
-                                temp.add(num);
-                                temp.add(numList[j]);
-                            } else {
-                                temp.add(numList[j]);
-                                temp.add(num);
-                            }
-                        } else {
-                            temp.add(numList[j]);
-                            if(num<numList[i]){
-                                temp.add(num);
-                                temp.add(numList[i]);
-                            } else {
-                                temp.add(numList[i]);
-                                temp.add(num);
-                            }
-                        }
-                        answer.add(temp);
-                    }
-                }
-            }
-        }
-        List<List<Integer>> resultList = new ArrayList<>(answer);
-        return resultList;
+        return new ArrayList<>(res);
     }
 }
